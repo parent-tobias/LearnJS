8f424748aba5070c4c91a5fea3f280586f023874
Chris <chriskirbydev@gmail.com>
2020-06-02 10:20:07 +1200

    Challenges with object notation

---
+++ js/objects.js
@@ -0,0 +1,37 @@
+// challenge
+// build a person with object literal notation
+// use three property values consisting of a string, number and boolean
+
+var humanBeing = {
+    hungry: true,
+    age: 25,
+    height: '5\'10'
+};
+
+// challenge
+// build an object with object literal notation
+// use dot notation to add four properties and values
+
+var pizza = {};
+
+Object.property = value;
+
+pizza.crust = 'wheat';
+
+pizza.sauce = 'mozzarella';
+
+pizza.toppings = 'cheese, pepperoni, olives, mushrooms';
+
+//challenge
+// add properties and values to an object with brack notation
+// use a boolean, string and number
+
+var cup = {};
+
+object['property'] = value;
+
+cup['insulated'] = true;
+
+cup['liquid'] = 'water';
+
+cup['oz'] = 16;
