f5debc0292edb93727c6d43716c9ef4f0ac19acf
Chris <chriskirbydev@gmail.com>
2020-06-02 10:39:04 +1200

    accessing objects with bracket and dot notation

--- js/objects.js
+++ js/objects.js
@@ -35,3 +35,45 @@ cup['insulated'] = true;
 cup['liquid'] = 'water';
 
 cup['oz'] = 16;
+
+// practice
+
+var snowman = {
+    isSnow: true,
+    color: 'white',
+    madeOfSnowballs: 3,
+    'first name': 'Frosty'
+};
+
+// challenge
+// access all the properties of object snowman. Start with dot notation
+
+snowman.isSnow;
+snowman.color: 
+snowman.madeOfSnowballs;
+
+// Change property values
+
+snowman.isSnow = 'yes';
+snowman.color = 'orange';
+snowman.madeOfSnowballs = 8;
+snowman;
+snowman['first name'] = 'Jack Frost';
+
+//challenge
+// build a house object
+// add 4 property values with dot notation with at least one value type of string, number and boolean
+// add 3 property values with brack notation with 3 property names that can only be added with bracket notation rather than dot notation
+
+
+var house = {};
+
+house.doors = 4;
+house.bedrooms = 3;
+house.address = '4219 S. Othello Street';
+house.office = false;
+house;
+house['swimming pool'] = false;
+house['1stOwner'] = 'Johm Doe';
+house['typeof'] = 'apartment';
+house;
