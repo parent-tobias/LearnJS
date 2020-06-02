f2e63142c7da88742c69714ff62dae58566bba96
Chris <chriskirbydev@gmail.com>
2020-06-02 14:55:48 +1200

    array methods

--- js/objects.js
+++ js/objects.js
@@ -77,3 +77,133 @@ house['swimming pool'] = false;
 house['1stOwner'] = 'Johm Doe';
 house['typeof'] = 'apartment';
 house;
+
+// practice
+
+var home = {
+    rooms: 4,
+    apartment: false,
+    color: 'brown',
+    swimmingPool: true,
+    state: 'WA',
+    city: 'Seattle'
+};
+
+var home2 = {
+    rooms: 2,
+    apartment: false,
+    color: 'brown',
+    swimmingPool: true,
+    state: 'CA',
+    city: 'Sacramento'
+};
+
+function Home(rooms, apartment, color, swimmingPool, state, city) {
+    this.rooms = rooms;
+    this.apartment = apartment;
+    this.color = color;
+    this.swimmingPool = swimmingPool;
+    this.state = state;
+    this.city = city;
+};
+
+var home1 = new Home(3, false, 'blue', true, 'Atlanta', 'GA');
+
+// challenge
+// build 3 more homes
+
+var home2 = new Home(2, true, 'brown', true, 'Cincinnati' 'OH');
+
+var home3 = new Home(3, false, 'red', true, 'Cleveland', 'OH');
+
+var home4 = new Home(4, false, 'blue', false, 'Miami', 'FL');
+
+// challenge
+// create book with object literal notation
+// create library with object constructor function
+// create 5 instance of book
+
+var book = {
+    author: 'Victor Hugo',
+    title: 'Les Miserables',
+    pages: 1200
+};
+
+function Book(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+};
+
+var book1 = new Book('Charles Dickenson', 'Tale of Two Cities', 567);
+
+var book2 = new Book('Definitive JavaScript', 'David Flanigan', 857);
+
+var book3 = new Book('Frankenstein', 'Mary Shelley', 454);
+
+// JS Array Syntax
+
+// practice
+
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros'];
+
+// challenge
+// add a value to the end of the array
+// turn 'Giants' into a different team
+// access the 'Dodgers' value and save it to variable myFavoriteTeam
+// access the length of the array
+
+baseballTeams[5] = 'Cardinals';
+
+baseballTeams[6] = 'Blue Jays';
+
+//outcome
+
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros', 'Cardinals', 'Blue Jays'];
+
+// Challenge change Giants
+
+baseballTeams[1] = 'Angels'
+
+baseballTeams[2] = 'Phillies';
+
+baseballTeams[0];
+
+varmyFavoriteTeam = baseballTeams[0];
+
+var californiaBaseball = baseballTeams[1];
+
+// array methods review
+// push() pop() shift() unshift()
+
+// practice
+
+var shoppingList = ['apples', 'piiza', 'chicken', 'mushrooms', 'oranges', 'beef'];
+
+// challenge
+// insert 'crackers' at the front of the array and 'grapes' at the end of the array
+
+shoppingList.unshift('crackers')
+shoppingList.push('grapes')
+
+["crackers", "apples", "piiza", "chicken", "mushrooms", "oranges", "beef", "grapes"]
+
+shoppingList.push('siracha', 'popcorn');
+
+shoppingList.unshift('lemonade', 'orange juice');
+
+// challenge
+// remove an item from the front and the end of the shopping list array
+
+var shoppingList = ["lemonade", "orange juice", "crackers", "apples", "piiza", "chicken", "mushrooms", "oranges", "beef", "grapes", "siracha", "popcorn"]
+
+shoppingList.pop();
+shoppingList.shift();
+
+
+var firstItemSarah = shoppingList.shift();
+
+var lastItemPhil = shoppingList.pop();
+
+["crackers", "apples", "piiza", "chicken", "mushrooms", "oranges", "beef", "grapes"]
+
