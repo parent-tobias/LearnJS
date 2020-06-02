30c5ee81c6c859a738d6a0c67ca750a49ea57768
Chris <chriskirbydev@gmail.com>
2020-06-02 16:20:29 +1200

    complete array lesson

--- js/objects.js
+++ js/objects.js
@@ -169,7 +169,7 @@ baseballTeams[2] = 'Phillies';
 
 baseballTeams[0];
 
-varmyFavoriteTeam = baseballTeams[0];
+var myFavoriteTeam = baseballTeams[0];
 
 var californiaBaseball = baseballTeams[1];
 
@@ -206,4 +206,50 @@ var firstItemSarah = shoppingList.shift();
 var lastItemPhil = shoppingList.pop();
 
 ["crackers", "apples", "piiza", "chicken", "mushrooms", "oranges", "beef", "grapes"]
+
+// review array methods
+// concat method - join two or more arrays
+// reverse method - reverse order of elements
+// sort method - sort alpha numeric
+// slice method -returns but does not delete a section
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// challenge
+// build another array and add the two together
+
+var colors2 = ['black', 'white', 'brown', 'purple'];
+
+var allColors = colors.concat(colors2);
+
+// challenge
+// add another array and use the concat method in two ways to add them together
+
+var colors3 = ['red', 'white', 'blue'];
+
+var moreAllColors = colors.concat(colors2, colors3);
+
+// challenge
+// reverse all the elements in the allColors array
+// sort all the elements in the allColors array by alphabetical order
+
+var reverseColors = allColors.reverse();
+
+var alphabetColors = allColors.sort();
+
+// challenge
+// create var winter and fill with winter values
+// create var summer and fill with summer values
+var weather = ['rainy', 'cold', 'chilly', 'snowy', 'cloudy', 'hot', 'warm', 'humid'];
+
+var winter = weather.slice(0, 5);
+
+var summer = weather(5);
+
+// challenge
+// create same variables using negative numbers
+
+var winter = weather.slice(-8, -3);
+
+var summer = weather.slice(-3,);
 
