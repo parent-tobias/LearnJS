2e7da55982f5adefe1a4b2e72c50f29eed14694f
Chris <chriskirbydev@gmail.com>
2020-06-24 17:33:26 +1200

    component state & props

---
+++ demo/src/Application.js
@@ -0,0 +1,53 @@
+import React, {Component} from "react";
+import Highscore from "./Highscore";
+import "./css/style.css";
+
+
+
+class Application extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }    
+
+    handleClick = () => {
+        this.setState({count: this.state.count + 1});
+    }
+
+    componentDidUpdate(props, state){
+        if(this.state.count > 10 && this.state.count != state.count && !this.state.overTen){
+            console.log("Updating over ten");
+            this.setState({overTen: true});
+        }
+    }
+
+    resetCount = (e) => {
+        this.setState({
+            count: 0,
+            overTen: false
+        });
+    }
+
+    render(){
+        let {count} = this.state;
+
+        return ( 
+            <div>
+                <h1>You clicked the button {count} times</h1>                
+                <Highscore
+                  overTen={this.state.overTen}
+                  onReset={this.resetCount}
+                />
+                <span>
+                    <button onClick={() => this.handleClick()}>Click Me</button>
+                </span>
+            </div> 
+        );
+    }
+}
+
+export default Application;
